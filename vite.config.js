1c37676485c573b8ad4fd1d33c782438a26ec7a4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-22 14:15:21 +0100

    chore: update setup

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,28 @@ import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
 import path from 'path';
 export default defineConfig({
-    server: {
-        port: 3500,
-        open: true,
+  server: {
+    port: 3500,
+    open: true,
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.ts'),
+      name: 'high-privacy-package-poc',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => `index.${format}.js`,
     },
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'index.ts'),
-            name: 'high-privacy-package-poc',
-            formats: ['es', 'cjs', 'umd'],
-            fileName: (format) => `index.${format}.js`,
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
-        rollupOptions: {
-            external: ['react', 'react-dom'],
-            output: {
-                globals: {
-                    react: 'React',
-                    'react-dom': 'ReactDOM',
-                },
-            },
-        },
-        sourcemap: true,
-        emptyOutDir: true,
+      },
     },
-    plugins: [react(), dts()],
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
 });
