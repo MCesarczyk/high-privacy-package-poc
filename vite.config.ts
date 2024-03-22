3ecdc3c2e8726a067cae219eac9fcd0b4bee5b7d
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-22 08:38:11 +0100

    chore(vitest): add, create first tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite';
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3500,
+    open: true,
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./setupTests.ts'],
+    environment: 'jsdom',
+  },
   plugins: [react()],
 });
