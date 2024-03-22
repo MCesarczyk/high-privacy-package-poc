44f3da35346af93ffd1b4007be28c44ffd3200eb
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-22 09:39:23 +0100

    build: configure params

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,7 +3,13 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+  ],
   overrides: [
     {
       env: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import path from 'path';
 
 export default defineConfig({
   server: {
@@ -12,5 +14,24 @@ export default defineConfig({
     setupFiles: ['./setupTests.ts'],
     environment: 'jsdom',
   },
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'high-privacy-package-poc',
+      formats: ['es', 'cjs', 'umd'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+  plugins: [react(), dts()],
 });
